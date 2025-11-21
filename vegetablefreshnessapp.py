58588d71f1170560fd2e6090acba6ac6ea887151
unknown <ankushtonde8@gmail.com>
2025-11-21 15:02:07 -0500

    Changed brackets for environment code

--- vegetablefreshnessapp.py
+++ vegetablefreshnessapp.py
@@ -58,9 +58,9 @@ uploaded_file = st.file_uploader(
 def load_freshness_model():
     s3 = boto3.client(
         "s3",
-        aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
-        aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
-        region_name=os.environ.get("AWS_DEFAULT_REGION", "us-east-2")
+        aws_access_key_id=os.environ.get["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key=os.environ.get["AWS_SECRET_ACCESS_KEY"],
+        region_name=os.environ.get["AWS_DEFAULT_REGION", "us-east-2"]
     )
 
     bucket_name = "veggies-freshness"
