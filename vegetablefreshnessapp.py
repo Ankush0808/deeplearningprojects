d8bbdd42060e9fd8dfbbe87dc86f1d8cb637b403
unknown <ankushtonde8@gmail.com>
2025-11-20 22:13:19 -0500

    changed the aws user permission policy

--- vegetablefreshnessapp.py
+++ vegetablefreshnessapp.py
@@ -58,15 +58,15 @@ uploaded_file = st.file_uploader(
 def load_freshness_model():
     s3 = boto3.client(
         "s3",
-        aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
-        aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
-        region_name=os.environ.get("AWS_DEFAULT_REGION", "us-east-1")
+        aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
+        region_name=os.environ.get("AWS_DEFAULT_REGION", "us-east-2")
     )
 
     bucket_name = "veggies-freshness"
     object_key = "freshness_model_pretrained.keras"
 
-    temp_file_path = os.path.join(tempfile.gettempdir(), "freshness_model.keras")
+    temp_file_path = os.path.join(tempfile.gettempdir(), "temppy.keras")
 
     # Download your model from S3
     s3.download_file(bucket_name, object_key, temp_file_path)
